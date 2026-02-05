2dd724dba46961c4014c0dbb72a330008c15fae3
Leon Gebler <22643729+FamousPig@users.noreply.github.com>
2026-02-05 15:23:02 +0100

    Fix typo in environment variables leading to connection failures

--- unitree_rosbox/dockerfiles/res/setup.sh
+++ unitree_rosbox/dockerfiles/res/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 source /opt/ros/humble/setup.bash
 source /opt/unitree_ros2/install/setup.bash
-export RMW_IMPLEMENTATINO=rmw_cyclonedds_cpp
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 #TODO query network interface on setup?
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
