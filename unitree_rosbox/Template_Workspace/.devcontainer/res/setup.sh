2b7503e8bd91bf9da08143aba3f0daf5610a4140
Leon Gebler <22643729+FamousPig@users.noreply.github.com>
2026-04-14 14:17:30 +0200

    Update devcontainer config in line with updated dockerfile

--- unitree_rosbox/Template_Workspace/.devcontainer/res/setup.sh
+++ unitree_rosbox/Template_Workspace/.devcontainer/res/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/env bash
 source /opt/ros/humble/setup.bash
-source /opt/unitree_ros2/install/setup.bash
+source /opt/unitree_ros2/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
-#TODO query network interface on setup?
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enp0s31f6" priority="default" multicast="default" />
+                            <NetworkInterface name="--INTERFACE--" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
