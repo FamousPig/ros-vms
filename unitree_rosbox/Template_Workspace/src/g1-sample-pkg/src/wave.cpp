b699884d46571e270a380a32dcea12e8a7907e4e
Leon Gebler <22643729+FamousPig@users.noreply.github.com>
2026-06-10 14:26:55 +0200

    Get started on adding sample package

---
+++ unitree_rosbox/Template_Workspace/src/g1-sample-pkg/src/wave.cpp
@@ -0,0 +1,14 @@
+
+#include "common/motor_crc_hg.h"
+
+class G1WaveSender : public rclcpp::Node {
+    public:
+      G1WaveSender(): Node("g1_wave_sender") {
+
+      }
+    private:
+};
+
+int main(int argc, char **argv) {
+    return 0;
+}
