6870c2bf003fe864537bf8d815d12ac32c68fdd1
Leon Gebler <22643729+FamousPig@users.noreply.github.com>
2026-07-08 14:40:59 +0200

    Source other underlay for better terminal ros use

--- unitree_rosbox/Template_Workspace/setup.sh
+++ unitree_rosbox/Template_Workspace/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Setup unitree ros2 simulation environment"
 source /opt/ros/humble/setup.bash
-source /opt/unitree_ros2/cyclonedds_ws/install/setup.bash
+source /opt/unitree_ros2/setup.sh
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
