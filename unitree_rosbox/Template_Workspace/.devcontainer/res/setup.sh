5c4ab169e128af1e307233ef7069bd4dcbe1e010
Leon Gebler <22643729+FamousPig@users.noreply.github.com>
2026-09-16 11:56:16 +0200

    Make sure unitree_sdk2 can be found by CMake

--- unitree_rosbox/Template_Workspace/.devcontainer/res/setup.sh
+++ unitree_rosbox/Template_Workspace/.devcontainer/res/setup.sh
@@ -3,6 +3,10 @@ source /opt/ros/humble/setup.bash
 source /opt/unitree_ros2/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
+if [[$CMAKE_PREFIX_PATH != *"/opt/unitree_robotics"*]]; then
+    export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/unitree_robotics
+fi
+
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="--INTERFACE--" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
